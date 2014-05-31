f55653206239da49e5d71af9c6ab692c9f521c6c
YanisJ <553124029@qq.com>
2014-05-31 22:08:25 +0800

    fix bugs

--- js/calculator.js
+++ js/calculator.js
@@ -37,7 +37,7 @@ for(var i=0; i<keys.length; i++){
 			decimalAdded = false;	
 		}
 		else if(btnVal=='.'){
-			if(!decimalAdd){
+			if(!decimalAdded){
 				input.innerHTML += btnVal;
 				decimalAdded = true;
 			}
